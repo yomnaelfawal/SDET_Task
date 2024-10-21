05fbe275a3cc0f33d089cb00ee790c2bf6310d34
Yomna Elfawal <youmnamandour23@gmail.com>
2024-10-21 22:02:45 +0300

    created a directory for files with different extensions to use on the file uploader tests, used a more descriptive name for the page object file for the contact us form

--- ui_tests/tests/contactUs.test.js
+++ ui_tests/tests/contactUs.test.js
@@ -1,13 +1,14 @@
 module.exports = {
     'Contact Us Test': function(browser){
-        const contactPage = browser.page.contactUs();
+        const contactPage = browser.page.contactUsForm();
 
         contactPage
             .navigate()
             .fillOutForm('test@hehe.com', '1200120012001200', 'hey its me im the problem its me')
             .makeSelection('WEBMASTER')
-            .uploadFiles("D:/Coding/Coursera/git-cheat-sheet-education.pdf")
+            .uploadFiles("./tests/dummy_files/mp3_test.mp3")
             .submit()
+            .pause(10000)
             .end();
     }
 }
